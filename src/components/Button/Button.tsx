3528805e9df6a593ea844b446023f00b1eeb2f86
Sito8943 <sito8943@gmail.com>
2023-11-02 19:16:27 -0700

    Button and Icon button as tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+import { forwardRef, ForwardedRef, HTMLProps } from "react";
+import Tippy from "@tippyjs/react";
+
+// styles
+import "./styles.css";
+
+export interface ButtonProps extends HTMLProps<HTMLButtonElement> {
+  tooltip: string;
+  type?: "button" | "submit" | "reset" | undefined;
+}
+
+const Button = forwardRef(function (
+  props: ButtonProps,
+  ref: ForwardedRef<HTMLButtonElement>
+) {
+  const { tooltip, type = "button", ...rest } = props;
+
+  if (!tooltip || !tooltip.length)
+    return (
+      <button
+        type={type}
+        ref={ref}
+        {...rest}
+        className={`button ${rest.className}`}
+      >
+        {rest.children}
+      </button>
+    );
+
+  return (
+    <Tippy content={tooltip}>
+      <button ref={ref} {...rest}>
+        {rest.children}
+      </button>
+    </Tippy>
+  );
+});
+
+export default Button;
--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,33 +1,32 @@
-import React from "react";
-import PropTypes from "prop-types";
-
 // font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 // components
-import Button from "../Button/Button";
+import Button, { ButtonProps } from "../Button/Button";
 
 // styles
 import "./styles.css";
+import { Ref, useRef } from "react";
 
-function IconButton(props) {
+interface IconButtonProps extends ButtonProps {
+  icon: IconProp;
+}
+
+function IconButton(props: IconButtonProps) {
+  const { icon, ...rest } = props;
+
+  const ref = useRef(rest.ref);
   return (
     <Button
-      {...props}
-      className={`hover:bg-pdark-hover icon-button ${props.className}`}
+      {...rest}
+      ref={ref as Ref<HTMLButtonElement>}
+      type="button"
+      className={`hover:bg-pdark-hover icon-button ${rest.className}`}
     >
-      <FontAwesomeIcon icon={props.icon} />
+      <FontAwesomeIcon icon={icon} />
     </Button>
   );
 }
-
-IconButton.defaultProps = {
-  type: "button",
-};
-
-IconButton.propTypes = {
-  type: PropTypes.string,
-  tooltip: PropTypes.string,
-};
 
 export default IconButton;
