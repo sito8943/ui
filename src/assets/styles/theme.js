06fa4cf1b8e9c7d7085bf7a0eda437be209b337c
Sito8943 <sito8943@gmail.com>
2023-11-12 16:19:16 -0800

    0.0.50

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,17 +1,13 @@
-module.exports = {
-  screens: {
+const theme = {
+  theme: {
     "2xl": { max: "1535px" },
     // => @media (max-width: 1535px) { ... }
-
     xl: { max: "1279px" },
     // => @media (max-width: 1279px) { ... }
-
     lg: { max: "1023px" },
     // => @media (max-width: 1023px) { ... }
-
     md: { max: "767px" },
     // => @media (max-width: 767px) { ... }
-
     sm: { max: "639px" },
     // => @media (max-width: 639px) { ... }
   },
@@ -46,3 +42,5 @@ module.exports = {
     info: "#0099CC",
   },
 };
+
+export default theme;
