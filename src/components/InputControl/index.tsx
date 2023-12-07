bca7af622b7ac06c31662e84ad51e643996f97d2
Sito8943 <sito8943@gmail.com>
2023-12-07 05:55:26 -0800

    inputs and types to index

---
+++ src/components/InputControl/index.tsx
@@ -0,0 +1,11 @@
+export type {
+  InputControlProps,
+  SelectControlProps,
+  TextareaControlProps,
+} from "./types";
+
+import TextareaControl from "./Textarea";
+import SelectControl from "./Select";
+import InputControl from "./Input";
+
+export { TextareaControl, SelectControl, InputControl };
