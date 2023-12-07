125287935b762166011ea3bc0f2082f96f53fd5e
Sito8943 <sito8943@gmail.com>
2023-12-07 07:29:40 -0800

    working on loading styles.ts

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -20,12 +20,12 @@ const Loading = (props: LoadingProps) => {
   const styles = makeStyles(colors, color, colorInverted);
 
   return (
-    <div {...rest} className={`loading ${styles} ${rest.className}`}>
+    <div {...rest} className={`loading ${styles.root} ${rest.className}`}>
       <div className="loader-container">
         <div className={`loader ${loaderClass}`}>
           <svg className="circular" viewBox="25 25 50 50">
             <circle
-              className={`path`}
+              className={`path ${styles.stroke}`}
               cx="50"
               cy="50"
               r="20"
