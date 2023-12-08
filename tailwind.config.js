92888378a690457121bbee104a7777b8b58e6a90
Sito8943 <sito8943@gmail.com>
2023-12-07 20:34:42 -0800

    remove colors from tailwind, using from Style

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,20 +24,6 @@ export default {
     colors: {
       dark: { dark: "#101010", default: "#303030", light: "#4e4e4e" },
       light: { dark: "#e1e1e1", default: "#ededed", light: "#f7f7f7" },
-      primary: {
-        light: "#F74037",
-        default: "#E83636", // <---
-        dark: "#D62C30",
-      },
-      secondary: {
-        light: "#008CFF",
-        default: "#0079F2", // <----
-        dark: "#0C67E0",
-      },
-      error: "#CC0000",
-      warning: "#FF8800",
-      success: "#007733",
-      info: "#0099CC",
     },
     extend: {},
   },
