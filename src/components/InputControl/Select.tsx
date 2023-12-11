355a0111f02500562ea149a81c8255b427ea09cc
Sito8943 <sito8943@gmail.com>
2023-12-10 16:21:35 -0800

    reduced Select

--- src/components/InputControl/Select.tsx
+++ src/components/InputControl/Select.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, ForwardedRef } from "react";
+import { forwardRef, ForwardedRef, useMemo } from "react";
 
 // providers
 import { useStyle } from "providers/StyleProvider";
@@ -26,6 +26,21 @@ const SelectControl = forwardRef(function (
   const { colors } = useStyle();
   const styles = makeStyles(colors, color);
 
+  const selectElement = useMemo(
+    () => (
+      <select
+        {...rest}
+        ref={ref as ForwardedRef<HTMLSelectElement>}
+        className={`${leftComponent ? "!pl-0" : ""} ${
+          rightComponent ? "!pr-0" : ""
+        } ${styles.input} ${rest.className ?? ""}`}
+      >
+        {rest.children}
+      </select>
+    ),
+    [leftComponent, rightComponent, rest, ref, styles.input]
+  );
+
   return (
     <div className={`input-control ${orientation} ${color}`}>
       <label htmlFor={props.id}>{label}</label>
@@ -34,27 +49,13 @@ const SelectControl = forwardRef(function (
           {leftComponent && leftComponent !== null ? (
             <div>{leftComponent}</div>
           ) : null}
-          <select
-            {...rest}
-            ref={ref as ForwardedRef<HTMLSelectElement>}
-            className={`${leftComponent ? "!pl-0" : ""} ${
-              rightComponent ? "!pr-0" : ""
-            } ${styles.input} ${rest.className ?? ""}`}
-          >
-            {rest.children}
-          </select>
+          {selectElement}
           {rightComponent && rightComponent !== null ? (
             <div>{rightComponent}</div>
           ) : null}
         </div>
       ) : (
-        <select
-          {...rest}
-          className={`${styles.input} ${rest.className ?? ""}`}
-          ref={ref as ForwardedRef<HTMLSelectElement>}
-        >
-          {rest.children}
-        </select>
+        selectElement
       )}
       {helperText && helperText.length ? <p>{helperText}</p> : null}
     </div>
