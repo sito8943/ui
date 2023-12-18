8ac974776e4902c7b03df434ed551530bf2b94c2
Sito8943 <sito8943@gmail.com>
2023-12-18 18:33:24 -0500

    added onVisible flag to PrintAfter

--- src/components/PrintAfter/PrintAfter.tsx
+++ src/components/PrintAfter/PrintAfter.tsx
@@ -1,16 +1,33 @@
 import { memo, useEffect, useState } from "react";
+import useIsInViewport from "use-is-in-viewport";
 import { PrintAfterProps } from "./types";
 
 function _PrintAfter(props: PrintAfterProps) {
-  const { children, delay = 100, animation = "appear" } = props;
+  const {
+    children,
+    delay = 100,
+    animation = "appear",
+    onVisible = false,
+  } = props;
+
+  const [isInViewport, targetRef] = useIsInViewport({ threshold: 50 });
+  const [visible, setVisible] = useState(false);
   const [see, setSee] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => setSee(true), delay);
+    const timer = setTimeout(() => {
+      if (onVisible) {
+        if (isInViewport) setVisible(true);
+      } else setSee(true);
+    }, delay);
     return () => clearTimeout(timer);
-  }, [delay]);
+  }, [delay, onVisible]);
 
-  return <div className={see ? animation : "invisible"}>{children}</div>;
+  return (
+    <div ref={targetRef} className={see && visible ? animation : "invisible"}>
+      {children}
+    </div>
+  );
 }
 
 const PrintAfter = memo(
@@ -23,7 +40,8 @@ function arePropsEqual(oldProps: PrintAfterProps, newProps: PrintAfterProps) {
   return (
     oldProps.delay === newProps.delay &&
     oldProps.animation === newProps.animation &&
-    oldProps.children === newProps.children
+    oldProps.children === newProps.children &&
+    oldProps.onVisible === newProps.onVisible
   );
 }
 
--- src/components/PrintAfter/types.ts
+++ src/components/PrintAfter/types.ts
@@ -2,4 +2,5 @@ export interface PrintAfterProps {
   children: React.ReactNode;
   delay?: number | undefined;
   animation?: "appear" | "aGrow" | "aShrink" | string | undefined;
+  onVisible?: boolean | undefined;
 }
