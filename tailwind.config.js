691a75e1d4a5b0ac262c1db111d5ac621a4d6863
Sito8943 <sito8943@gmail.com>
2023-12-19 18:06:20 -0500

    better breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
+      "2xl": { min: "1537px" },
+      // => @media (min-width: 1535px) { ... }
 
-      xl: { max: "1279px" },
+      xl: { max: "1536px" },
       // => @media (max-width: 1279px) { ... }
 
-      lg: { max: "1023px" },
+      lg: { max: "1200px" },
       // => @media (max-width: 1023px) { ... }
 
-      md: { max: "767px" },
+      md: { max: "900px" },
       // => @media (max-width: 767px) { ... }
 
-      sm: { max: "639px" },
+      sm: { max: "600px" },
       // => @media (max-width: 639px) { ... }
 
-      xs: { max: "383px" },
+      xs: { max: "400px" },
       // => @media (max-width: 383px) { ... }
     },
     colors: {
