342a05b6bb0856bc8344528fb95ed06c947d3ebc
Sito8943 <sito8943@gmail.com>
2026-07-08 17:16:29 +0200

    updated packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import react from "@vitejs/plugin-react";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
 import dts from "vite-plugin-dts";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const srcPath = resolve(__dirname, "src");
+const external = [
+  "react",
+  "react-dom",
+  "react/jsx-runtime",
+  "react/jsx-dev-runtime",
+];
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -17,13 +26,14 @@ export default defineConfig({
   plugins: [react(), dts({ insertTypesEntry: true }), libInjectCss()],
   build: {
     copyPublicDir: false,
+    target: "es2020",
     rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
+      external,
     },
     lib: {
       entry: resolve(__dirname, "src/main.ts"),
-      name: '@sito/ui',
-      fileName: 'ui',
+      name: "@sito/ui",
+      fileName: "ui",
       formats: ["es", "cjs"],
     },
   },
